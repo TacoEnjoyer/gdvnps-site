b6baaaf0e26b26a42eec2eccc2fc59046abd21e1
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2024-10-30 05:48:37 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import vue from '@astrojs/vue';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind(), react()]
 });
