57bbcdbafef12f75ea94c2c494ed73e525c91c64
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2024-10-30 17:43:58 +0700

    update

--- src/components/views/index.ts
+++
@@ -1,6 +0,0 @@
-import HomeView from './HomeView.vue'
-import DownloadView from './DownloadView.vue'
-import FAQView from './FAQView.vue'
-import NotFoundView from './NotFoundView.vue'
-
-export { HomeView, DownloadView, FAQView, NotFoundView }
